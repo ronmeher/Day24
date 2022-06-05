0e0722633c985d39656c83bfcaafbc558d24983a
ronmeher <ronmeher@gmail.com>
2022-06-05 15:51:07 +0530

    Day 24 commmit

---
+++ LambdaExpression.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationUsingLAMBDAExpression
+{
+    public class LambdaExpression
+    {
+        //Validating Userinput
+        public void Validate()
+        {
+            string[] RegexPattern = new string[6];
+
+            //For First name
+            RegexPattern[0] = "^[A-Z][A-Za-z]{2,}$";
+
+            //For Last Name
+            RegexPattern[1] = "^[A-Z][A-Za-z]{2,}$";
+
+            //For Email id
+            RegexPattern[2] = "^[A-Z0-9a-z]{1,}([.#$^][A-Za-z0-9]+)?[@][A-Za-z]{2,}[.][A-Za-z]{2,3}([.][a-zA-Z]{2})?$";
+
+            //For Phone Number
+            RegexPattern[3] = "^[1-9]{2,}[ ][0-9]{10}$";
+
+            //For Password
+            RegexPattern[4] = "^[A-Za-z0-9]*[!@#$%^$*]{1}([a-zA-Z0-9])*$";
+
+            string[] UserInput = new string[6];
+
+            //Geting User in
+            Console.WriteLine("Enter First Name:");
+            UserInput[0] = Console.ReadLine();
+
+            Console.WriteLine("Enter Last Name:");
+            UserInput[1] = Console.ReadLine();
+
+            Console.WriteLine("Enter Email Id:");
+            UserInput[2] = Console.ReadLine();
+
+            Console.WriteLine("Enter Phone Number:");
+            UserInput[3] = Console.ReadLine();
+
+            Console.WriteLine("Enter passward:");
+            UserInput[4] = Console.ReadLine();
+
+            Console.WriteLine("\nResults are: ");
+            //Lambda Expression
+            for (int i = 0; i < 6; i++)
+            {
+                Func<string, bool> nameValidater = UserInput => Regex.IsMatch(UserInput, RegexPattern[i]);
+                bool result = nameValidater(UserInput[i]);
+                Console.WriteLine(($"{i + 1} {UserInput[i]} : {result}"));
+
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UserRegistrationUsingLAMBDAExpression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration Using Lambda Expression\n\n");
+            LambdaExpression lambdaExpression = new LambdaExpression();
+            lambdaExpression.Validate();
+        }
+    }
+}
